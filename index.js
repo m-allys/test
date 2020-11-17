f1b15c00c2c6d8a5927c27319ac58dfd50ee031a
VISION <38427929+m-allys@users.noreply.github.com>
2020-11-17 21:37:39 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+'use strict';
+
+const express = require('express');
+
+const Main = class {};
+
+Main.start = (function(){
+   
+   let server = express()
+       .get("/", (req, res) => {
+           res.send("<h1>  HELLO TEST ! </h1>");
+       });
+       
+   server.listen(process.env.PORT || 5000);
+});
